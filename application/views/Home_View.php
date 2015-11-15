2988cee68770ebdd12ad4a98f093ee5321464567
nilupull <nilupulliyanage17@gmail.com>
2015-11-15 20:06:04 +0530

    Adding form names

--- application/views/Home_View.php
+++ application/views/Home_View.php
@@ -4,7 +4,7 @@
             <h1>Welcome to Take-A-Quiz</h1>
             <p class="lead">please select one of the following quizzes</p>
             <br>
-            <form class="col-lg-12" action="<?php echo base_url(); ?>question_Controller" method="POST">
+            <form class="col-lg-12" name="Home_Form" action="<?php echo base_url(); ?>question_Controller" method="POST">
                 <div class="input-group input-group-lg col-sm-offset-4 col-sm-4">
                     <select type="text" class="center-block form-control input-lg" title="Select a quiz" name="quizDropDown" id="quizDropDown">
                         <!--Dynamically loading options of this drop-down
